22ba495dc00c25baefa8780698641f39feb1cd74
Ejub Hadzic <ejub@tayra.io>
2021-10-04 12:39:10 +0200

    add humidity to forecast output

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -12,7 +12,7 @@ const forecast = (latitude, longitude, callback) => {
     } else {
       callback(
         undefined,
-        `${current.weather_descriptions[0]}. It is currently ${current.temperature} degress out. It feels like ${current.feelslike} degress out.`
+        `${current.weather_descriptions[0]}. It is currently ${current.temperature} degress out. It feels like ${current.feelslike} degress out. And humidity is ${current.humidity}%`
       );
     }
   });
